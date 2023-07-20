7e881796a0dc9e9ebfcfc0050bb838dc7f9edf7d
Farhad-Tanveee <farhadtanveer05u@example.com>
2023-07-20 20:05:14 +0200

    added benefit page

--- src/shared/Htext.tsx
+++ src/shared/Htext.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
-type Props = {};
+type Props = {
+  children: React.ReactNode;
+};
 
-const Htext = (props: Props) => {
-  return <div>Htext</div>;
+const Htext = ({ children }: Props) => {
+  return (
+    <div>
+      <h1 className=" basis-3/5 font-montserrat text-3xl font-bold">
+        {children}
+      </h1>
+    </div>
+  );
 };
 
 export default Htext;
